6af487209b9eefde96ba3b619682b0d1e682af6a
ganesh-redy <ganeshreddy7610@gmail.com>
2025-03-02 19:25:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,26 @@
 provider "google" {
-    project = "sam-452209"
-    zone = "us-central1"
-
-  
+  project = "sam-452209"
+  region  = "us-central1"  # Add region
+  zone    = "us-central1-a" # Specify the zone
 }
+
 locals {
-  l1= ["instance1","instance2"]
-
+  l1 = ["instance1", "instance2"]
 }
-resource "google_compute_instance" "inst1" {
-    count = length(local.l1)
-    name = local.l1[count.index]
-    machine_type = "e2-medium"
-    boot_disk {
-      initialize_params {
-        image = "centos-stream-9"
 
-      }
-      
+resource "google_compute_instance" "inst1" {
+  count        = length(local.l1)
+  name         = local.l1[count.index]
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "projects/centos-cloud/global/images/family/centos-stream-9"
     }
-  
+  }
+
   network_interface {
     network = "default"
-    access_config {
-      
-    }
+    access_config {}  # This enables external IP
   }
 }
